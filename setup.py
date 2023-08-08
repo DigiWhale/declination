22e0f029d6a816683df51743e4e9dffedc40e5ef
digiwhale <digiwhale.humphrey@gmail.com>
2023-08-08 12:42:57 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='declination',
-    version='1.0',
+    version='1.0.2',
     description='Calculates declination given a latitude and longitude.',
-    packages=find_packages(),
-    author='Andrew Humphrey',  # Replace with your name
-    author_email='digiwhale.humphrey@gmail.com',  # Replace with your email
-    license='MIT',  # Assuming MIT, but replace if different
-    url='https://github.com/DigiWhale/declination',  # Replace with your repository or homepage URL
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={
+        'declination': ['WMM.COF'],
+    },
+    author='Andrew Humphrey',
+    author_email='digiwhale.humphrey@gmail.com',
+    license='MIT',
+    url='https://github.com/DigiWhale/declination',
     install_requires=[
         # No third-party dependencies based on the imports you've mentioned
     ],
